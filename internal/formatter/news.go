aa93ed0d5076f9488b4ad4a69baa1d98657397ce
Aleksandr Bortnikov <albortnikov@ozon.ru>
2026-04-01 12:49:53 +0300

    fix: trying to fix news issue

--- internal/formatter/news.go
+++ internal/formatter/news.go
@@ -3,6 +3,7 @@ package formatter
 import (
 	"fmt"
 	"io"
+	"strings"
 
 	"github.com/Alexandersfg4/crypto-analyzer/internal/models"
 	"github.com/samber/lo"
@@ -11,15 +12,19 @@ import (
 func News(w io.Writer, gotNews models.GetNewsResponse) {
 	fmt.Fprintln(w, "🔥 *Top News*")
 	for i, news := range gotNews {
-		fmt.Fprintf(w, "%d. `%s`\n", i+1, news.Title)
+		fmt.Fprintf(w, "%d. [%s](%s)\n", i+1, clearFromInvalidChars(news.Title), news.Link)
 		coins := make(map[string]bool, len(news.Coins))
 		for _, coin := range news.Coins {
 			coins[fmt.Sprintf("`%s`", coin.CoinNameKeyWords)] = true
 		}
 		if len(coins) > 0 {
-			fmt.Fprintln(w, "	Affected coins: ", lo.Keys(coins))
+			fmt.Fprintln(w, "Affected coins: ", lo.Keys(coins))
 		}
 	}
 
 	fmt.Fprintln(w)
+}
+
+func clearFromInvalidChars(s string) string {
+	return strings.ReplaceAll(s, ".", "")
 }
