caa6ff413b5af100e7bb36928c1d6772151bccaa
Aleksandr Bortnikov <albortnikov@ozon.ru>
2026-06-07 14:02:17 +0300

    feat: implementeed rate limiter for coinstats

--- internal/providers/coinstats/provider.go
+++ internal/providers/coinstats/provider.go
@@ -1,16 +1,5 @@
 package coinstats
 
-import (
-	"net/http"
-)
-
 func New(apiKey string) *Service {
-	return &Service{
-		httpClient: &http.Client{
-			Transport: &rt{
-				baseURL: baseURL,
-				apiKey:  apiKey,
-			},
-		},
-	}
+	return NewService(apiKey)
 }
--- internal/providers/coinstats/service.go
+++ internal/providers/coinstats/service.go
@@ -4,19 +4,24 @@ import (
 	"fmt"
 	"net/http"
 	"net/url"
+	"sync"
+	"time"
 )
 
 const (
-	apiHeader = "X-API-KEY"
-	baseURL   = "https://openapiv1.coinstats.app"
+	apiHeader                = "X-API-KEY"
+	baseURL                  = "https://openapiv1.coinstats.app"
+	defaultRateLimitInterval = 500 * time.Millisecond
 )
 
 func NewService(apiKey string) *Service {
 	return &Service{
 		httpClient: &http.Client{
 			Transport: &rt{
-				baseURL: baseURL,
-				apiKey:  apiKey,
+				baseURL:     baseURL,
+				apiKey:      apiKey,
+				rateLimit:   defaultRateLimitInterval,
+				lastRequest: time.Time{},
 			},
 		},
 	}
@@ -27,11 +32,17 @@ type Service struct {
 }
 
 type rt struct {
-	baseURL string
-	apiKey  string
+	baseURL   string
+	apiKey    string
+	rateLimit time.Duration
+
+	mu          sync.Mutex
+	lastRequest time.Time
 }
 
 func (rt *rt) RoundTrip(req *http.Request) (*http.Response, error) {
+	rt.waitRateLimit()
+
 	baseURL, err := url.Parse(rt.baseURL)
 	if err != nil {
 		return nil, fmt.Errorf("failed to parse base URL: %w", err)
@@ -43,4 +54,21 @@ func (rt *rt) RoundTrip(req *http.Request) (*http.Response, error) {
 	req.URL = gotURL
 	req.Header.Set(apiHeader, rt.apiKey)
 	return http.DefaultTransport.RoundTrip(req)
+}
+
+func (rt *rt) waitRateLimit() {
+	if rt.rateLimit <= 0 {
+		return
+	}
+
+	rt.mu.Lock()
+	defer rt.mu.Unlock()
+
+	if !rt.lastRequest.IsZero() {
+		if wait := rt.rateLimit - time.Since(rt.lastRequest); wait > 0 {
+			time.Sleep(wait)
+		}
+	}
+
+	rt.lastRequest = time.Now()
 }
